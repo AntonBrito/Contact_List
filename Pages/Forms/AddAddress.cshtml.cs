3835be06346e54c06ff29a52374b5889bc567446
Antonio Alexan Brito <antonioalexanbrito@Antonios-MacBook-Pro.local>
2021-06-07 16:10:51 -0400

    Added AddAddress page

---
+++ Pages/Forms/AddAddress.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContactLista.Pages.Forms
+{
+    public class AddAddressModel : PageModel
+    {
+
+        [BindProperty]
+
+        public AddAddressModel Address { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
